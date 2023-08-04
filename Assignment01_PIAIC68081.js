e77dc8ed3783209547ef0a8ea57685f45c2d6181
Umair-Munir-1991 <140552953+Umair-Munir-1991@users.noreply.github.com>
2023-08-04 22:30:56 +0500

    Add files via upload

---
+++ Assignment01_PIAIC68081.js
@@ -0,0 +1,29 @@
+"use strict";
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT");
+var My_Name = ("My name is Umair Munir");
+console.log(My_Name);
+var num_1 = (1);
+var num_2 = (2);
+var num_3 = (3);
+var num_4 = (4);
+var num_5 = (5);
+var num_6 = (6);
+var num_7 = (7);
+var num_8 = (8);
+var num_9 = (9);
+var num_10 = (10);
+console.log("ADDITION OF ALL NUMBERS");
+var ADD = (num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8 + num_9 + num_10);
+console.log(ADD); //Result of Adding num_1 to num_10//
+console.log("DIFFERENCE OF ALL NUMBERS");
+var DIFF = (num_1 - num_2 - num_3 - num_4 - num_5 - num_6 - num_7 - num_8 - num_9 - num_10);
+console.log(DIFF); //Result of Subtraction num_1 to num_2//
+console.log("MULTIPLICATION OF ALL NUMBERS");
+var MULTI = (num_1 * num_2 * num_3 * num_4 * num_5 * num_6 * num_7 * num_8 * num_9 * num_10);
+console.log(MULTI); //Result of Multiplication num_1 to num_10//
+console.log("Division of two_numbers");
+var num_2_div_num_3 = (num_2 / num_3);
+console.log(num_2_div_num_3); // Divsion of just two number i.e, num_2/num_3//
+console.log("DIVISION OF ALL NUMBERS");
+var DIV = (num_1 / num_2 / num_3 / num_4 / num_5 / num_6 / num_7 / num_8 / num_9 / num_10);
+console.log(DIV); //Result of Division of num_1 to num_10//
---
+++ Assignment01_PIAIC68081.ts
@@ -0,0 +1,34 @@
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT");
+var My_Name : string =("My name is Umair Munir");
+console.log(My_Name);
+var num_1: number =(1);
+var num_2: number =(2);
+var num_3: number =(3);
+var num_4: number =(4);
+var num_5: number =(5);
+var num_6: number =(6);
+var num_7: number =(7);
+var num_8: number =(8);
+var num_9: number =(9);
+var num_10:number =(10);
+console.log("ADDITION OF ALL NUMBERS");
+var ADD : number =(num_1+num_2+num_3+num_4+num_5+num_6+num_7+num_8+num_9+num_10);
+console.log(ADD);                    //Result of Adding num_1 to num_10//
+console.log("DIFFERENCE OF ALL NUMBERS");
+var DIFF : number =(num_1-num_2-num_3-num_4-num_5-num_6-num_7-num_8-num_9-num_10);
+console.log(DIFF);                  //Result of Subtraction num_1 to num_2//
+console.log("MULTIPLICATION OF ALL NUMBERS");
+var MULTI :number=(num_1*num_2*num_3*num_4*num_5*num_6*num_7*num_8*num_9*num_10);
+console.log(MULTI);                //Result of Multiplication num_1 to num_10//
+console.log("Division of two_numbers");
+var num_2_div_num_3: number= (num_2/num_3);
+console.log(num_2_div_num_3);     // Divsion of just two number i.e, num_2/num_3//
+console.log("DIVISION OF ALL NUMBERS");
+var DIV :number = (num_1/num_2/num_3/num_4/num_5/num_6/num_7/num_8/num_9/num_10);
+console.log(DIV);                 //Result of Division of num_1 to num_10//
+
+
+
+
+
+
